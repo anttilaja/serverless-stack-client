1ba2dd840ae81707e0cdad39c99580a874c7998f
7ubuntu user <ubunturemote@ip-172-31-2-214.eu-north-1.compute.internal>
2020-10-15 15:43:46 +0300

    Note app started

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,61 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch} from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import ResetPassword from "./containers/ResetPassword";
+import UnauthenticatedRoute  from "./components/UnauthenticatedRoute";
+import AuthenticatedRoute  from "./components/AuthenticatedRoute";
+import ChangePassword from "./containers/ChangePassword";
+import ChangeEmail from "./containers/ChangeEmail";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
+
+
+
+
+
+
+
+
+
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <AuthenticatedRoute exact path="/notes/new">
+      <NewNote />
+    </AuthenticatedRoute>
+    <AuthenticatedRoute exact path="/notes/:id">
+      <Notes />
+    </AuthenticatedRoute>
       {/* Finally, catch all unmatched routes */}
+	  <UnauthenticatedRoute exact path="/login">
+  			<Login />
+	  </UnauthenticatedRoute>
+    <UnauthenticatedRoute exact path="/signup">
+      <Signup />
+    </UnauthenticatedRoute>
+    <AuthenticatedRoute exact path="/settings">
+        <Settings />
+    </AuthenticatedRoute>
+    <UnauthenticatedRoute exact path="/login/reset">
+        <ResetPassword />
+    </UnauthenticatedRoute>
+    <AuthenticatedRoute exact path="/settings/email">
+      <ChangeEmail />
+    </AuthenticatedRoute>
+    <AuthenticatedRoute exact path="/settings/password">
+      <ChangePassword />
+    </AuthenticatedRoute>
 	  <Route>
 		 <NotFound />
       </Route>
-
     </Switch>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY: "pk_test_51HRZgDJi97rcgRJiYqvh0jBrp1h1NyRzLwRVvzJjQJMWozwrq0BGEce3lj9jpuVKweNN0nky06QejwJV1KBCgwuq00Ig1gMuvW",
   s3: {
     REGION: "eu-north-1",
-    BUCKET: "notes-app-upload-serverless-app"
+    BUCKET: "notes-app-api-2-prod-serverlessdeploymentbucket-42qj5slmqqaq"
   },
   apiGateway: {
     REGION: "eu-north-1",
-    URL: "https://uadaimga60.execute-api.eu-north-1.amazonaws.com/prod"
+    URL: "https://5u72hsyh57.execute-api.eu-north-1.amazonaws.com/prod"
   },
   cognito: {
     REGION: "eu-west-2",
     USER_POOL_ID: "eu-west-2_tmarEAa98",
     APP_CLIENT_ID: "6kb14bciqu5gd2psot31sog8us",
-    IDENTITY_POOL_ID: "eu-west-2:89cec5ea-86e5-4758-be89-0eec4927db2b"
+    IDENTITY_POOL_ID: "eu-west-2:47286cac-72cb-4927-bc14-f280b7739980"
   }
 };
