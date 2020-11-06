1d07baa122cf78694f829a03972e85857dcdf256
7ubuntu user <ubunturemote@ip-172-31-2-214.eu-north-1.compute.internal>
2020-11-06 12:45:57 +0200

    Updating the lander

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import { LinkContainer } from "react-router-bootstrap";
@@ -58,14 +59,23 @@ export default function Home() {
 }
 
 
-  function renderLander() {
-    return (
-      <div className="lander">
-        <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+ function renderLander() {
+  return (
+    <div className="lander">
+      <h1>Scratch</h1>
+      <p>A simple note taking app</p>
+      <div>
+        <Link to="/login" className="btn btn-info btn-lg">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-success btn-lg">
+          Signup
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
 
   function renderNotes() {
     return (
