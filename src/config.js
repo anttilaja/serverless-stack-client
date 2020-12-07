29a2e9eb864ea2bdf6f55e104e151a0f5e25a35f
7ubuntu user <ubunturemote@ip-172-31-2-214.eu-north-1.compute.internal>
2020-12-07 16:57:18 +0200

    api end points changed

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ default_temp {
     APP_CLIENT_ID: "6kb14bciqu5gd2psot31sog8us",
     IDENTITY_POOL_ID: "eu-west-2:47286cac-72cb-4927-bc14-f280b7739980"
   }
-};*/
+};
 
 const dev = {
  STRIPE_KEY: "pk_test_51HRZgDJi97rcgRJiYqvh0jBrp1h1NyRzLwRVvzJjQJMWozwrq0BGEce3lj9jpuVKweNN0nky06QejwJV1KBCgwuq00Ig1gMuvW",
@@ -32,7 +32,7 @@ const dev = {
     REGION: "eu-west-2",
     USER_POOL_ID: "eu-west-2_NqNqT5yfA",
     APP_CLIENT_ID: "6ea4loef5s22oq079vssqtnm6v",
-    IDENTITY_POOL_ID: "eu-west-2:9e74db9e-a8c3-4887-81b7-5b973c6af647"
+    IDENTITY_POOL_ID: "eu-west-2:9e74db9e-a8c3prod-notes-infra-s3-uploads4f6eb0fd-em4iy7qlfldy-4887-81b7-5b973c6af647"
   },
   social: {
     FB: "702023680442825"
@@ -58,12 +58,57 @@ const prod = {
   social: {
     FB: "702023680442825"
   }
+};   */
+
+
+
+const dev = {
+ STRIPE_KEY: "pk_test_51HRZgDJi97rcgRJiYqvh0jBrp1h1NyRzLwRVvzJjQJMWozwrq0BGEce3lj9jpuVKweNN0nky06QejwJV1KBCgwuq00Ig1gMuvW",
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "dev-notes-infra-s3-uploads4f6eb0fd-zf2fkonvsrqc"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://bsjeqihzte.execute-api.eu-west-2.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_xbKOAdq4j",
+    APP_CLIENT_ID: "338itjn2tk8gal63ii5454fm9j",
+    IDENTITY_POOL_ID: "eu-west-2:0e871c26-b29e-4946-ac82-d64013bba6e5"
+  },
+  social: {
+    FB: "702023680442825"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_51HRZgDJi97rcgRJiYqvh0jBrp1h1NyRzLwRVvzJjQJMWozwrq0BGEce3lj9jpuVKweNN0nky06QejwJV1KBCgwuq00Ig1gMuvW",
+  s3: {
+    REGION: "eu-west-2",
+    BUCKET: "prod-notes-infra-s3-uploads4f6eb0fd-k39cpuqr1gkh"
+  },
+  apiGateway: {
+    REGION: "eu-west-2",
+    URL: "https://egs0l327v4.execute-api.eu-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-west-2",
+    USER_POOL_ID: "eu-west-2_J7Y2Ahlcr",
+    APP_CLIENT_ID: "2lj7b4emiap2ftvcgvhp0uug98",
+    IDENTITY_POOL_ID: "eu-west-2:77db5354-010b-4580-9ea1-590a735583eb"
+  },
+  social: {
+    FB: "702023680442825"
+  }
 };
 
 // Default to dev if not set
 const config = process.env.REACT_APP_STAGE === 'prod'
   ? prod
   : dev;
+
 
 export default {
   // Add common config values here
